dd9685b4f333e4e5175d7a1ab71289268c12c623
DanProença@DeviLab <devillabdeveloper@gmail.com>
2023-02-22 14:38:35 -0300

    Adaptando para rodar com serverless no Netlify

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 const Myapp = require('./express/server.js')
 
 
-Myapp.listen(8080, () => console.log('Local app listening on port 8080!'));
+Myapp.listen(3000, () => console.log('Local app listening on port 3000!'));
 
